0ddf28d6de025bb5c8e12d8e696d2bed51a96144
sierraskyewallace <sskwallace@gmail.com>
2022-01-04 17:26:48 -0800

    moved actions to correxct file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';  
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import recipeReducer from './reducers/recipeReducer';
 
 
-const store = createStore(reducer);
+
+const store = createStore(recipeReducer);
+
+
 
