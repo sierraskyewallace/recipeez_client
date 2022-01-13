16542c0c2b605f17b88a586e465d437db1963927
sierraskyewallace <sskwallace@gmail.com>
2022-01-13 08:41:28 -0800

    setting up redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
+
+
+ReactDOM.render(
+  <Provider >
+    <App />
+  </Provider>
+  , document.getElementById("root"));
+  
 
