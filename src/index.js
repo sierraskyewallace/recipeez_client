421f78db96fa352744434f91e3cc052d61f846a1
sierraskyewallace <sskwallace@gmail.com>
2022-01-13 09:14:12 -0800

    recipe reducer laid out and passed in store

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
+import recipeReducer from "./reducers/recipeReducer";
 
 
 import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-
+let store = createStore(recipeReducer, composeEnhancers(applyMiddleware(thunk)));
+ 
 ReactDOM.render(
   <Provider store={store}>
     <App />
---
+++ src/reducers/recipeReducer.js
@@ -0,0 +1,6 @@
+
+
+export default function recipeReducer(state = {recipes: []}, action) {
+    return state;
+}
+
